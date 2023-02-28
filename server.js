fe5bcbd600afc9d9ef3a9dd19cadcf13af6e1057
Christopher Suzukida <chris.suzukida@gmail.com>
2023-02-27 17:17:41 -0800

    Create and connect postgreSQL db to server

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const { Pool } = require('pg');
+
+const dbPassword = process.env.DB_PASSWORD;
 
 const app = express();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'SQL Test',
+  password: dbPassword,
+  port: 5432,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 
 app.use(express.json());
 app.use(
