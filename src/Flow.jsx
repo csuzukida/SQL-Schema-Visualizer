e20dfe8c59702d81394258250f544da36cbf8cc2
Christopher Suzukida <chris.suzukida@gmail.com>
2023-03-02 01:32:52 -0800

    Create group nodes and children nodes

--- src/Flow.jsx
+++ src/Flow.jsx
@@ -35,9 +35,26 @@ function Flow() {
       try {
         const response = await axios.get('/api/schemas');
         const schemaData = response.data.result;
-
         const allNodes = [];
 
+        // Generate the group nodes
+        schemaData.forEach((schema) => {
+          allNodes.push({
+            id: schema.table_name + 1,
+            type: 'group',
+            position: { x: Math.floor(Math.random() * 800), y: Math.floor(Math.random() * 800) },
+            style: {
+              background: '#fff',
+              border: '1px solid #000',
+              padding: 10,
+              borderRadius: 5,
+              width: 200,
+              height: 200,
+            },
+          });
+        });
+
+        // Generate the table nodes as children of the group nodes
         schemaData.forEach((schema, index) => {
           if (!allNodes.includes(schema.table_name)) {
             allNodes.push({
@@ -45,12 +62,24 @@ function Flow() {
               data: { label: schema.table_name },
               position: { x: 250 + index * 30, y: 250 + index * 30 },
               type: 'input',
+              parentNode: schema.table_name + 1,
+              extent: 'parent',
             });
           }
         });
 
-        console.log('PARENT_NODES', allNodes);
-        setNodes(allNodes);
+        const testNode = {
+          id: 'test',
+          data: { label: 'test' },
+          position: { x: 250, y: 250 },
+          type: 'input',
+          parentNode: 'people',
+          extent: 'parent',
+        };
+
+        const testNodes = allNodes.concat(testNode);
+
+        setNodes(testNodes);
       } catch (err) {
         console.log(err);
       }
